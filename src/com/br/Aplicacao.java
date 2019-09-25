1b4f792dd6ce1f73b7673380325ab4d87ea4875f
Lucas <lucasrchaves1@gmail.com>
2019-09-25 01:51:02 -0300

    Covertendo CSV pra XML

--- src/com/br/Aplicacao.java
+++ src/com/br/Aplicacao.java
@@ -1,11 +1,17 @@
 package com.br;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -49,6 +55,23 @@ public class Aplicacao {
 			// LER DADOS DO CSV
 			lerDados(leitorCsv, document, raiz);
 			
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	        Transformer transf = transformerFactory.newTransformer();
+	        
+	        transf.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+	        transf.setOutputProperty(OutputKeys.INDENT, "yes");
+	        transf.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+	        
+	        
+	        // GRAVAR DADOS NO ARQUIVO XML
+	        DOMSource source = new DOMSource(document);
+	        File myFile = new File(FILE_XML);
+	        StreamResult file = new StreamResult(myFile);
+	        transf.transform(source, file);
+	        
+	        leitorCsv.close();
+			
+			
 		}catch (Exception e) {
 			// TODO: handle exception
 		}
@@ -81,49 +104,42 @@ public class Aplicacao {
 			Element crime = document.createElement("crime");
 			
 			
-//			crime.appendChild(createElement(document, "numero", data[0])); 
-//			crime.setId(data[1]);
-//			crime.setCase_number(data[2]);
-//			crime.setDate(data[3]);
-//			crime.setBlock(data[4]);
-//			crime.setIucr(data[5]);
-//			crime.setPrimary_type(data[6]);
-//			crime.setDescription(data[7]);
-//			crime.setArrest(data[8]);
-//			crime.setDomestic(data[9]);
-//			crime.setBeat(data[10]);
-//			crime.setDistrict(data[11]);
-//			crime.setWard(data[12]);
-//			crime.setFbi_code(data[13]);
-//			crime.setYear(data[14]);
-//			crime.setUpdated_on(data[15]);
-			
+			crime.appendChild(createElement(document, "numero", data[0])); 
+			crime.appendChild(createElement(document, "id", data[1]));
+			crime.appendChild(createElement(document, "case_number", data[2]));
+			crime.appendChild(createElement(document, "date", data[3]));
+			crime.appendChild(createElement(document, "block", data[4]));
+			crime.appendChild(createElement(document, "iucr", data[5]));
+			crime.appendChild(createElement(document, "primary_type", data[6]));
+			crime.appendChild(createElement(document, "description", data[7]));
+			crime.appendChild(createElement(document, "arrest", data[8]));
+			crime.appendChild(createElement(document, "domestic", data[9]));
+			crime.appendChild(createElement(document, "beat", data[10]));
+			crime.appendChild(createElement(document, "district", data[11]));
+			crime.appendChild(createElement(document, "ward", data[12]));
+			crime.appendChild(createElement(document, "fbi_code", data[13]));
+			crime.appendChild(createElement(document, "year", data[14]));
+			crime.appendChild(createElement(document, "updated_on", data[15]));
 			
 
-//			Element rimeElement = document.createElement("crime");
-//			Element rimeElement = document.createElement("localizacao");
-//			
-//			Element nodeNumero = document.createElement("numero");
-//			nodeNumero.appendChild(document.createTextNode(crime.getNumero()));
-//
-//			Element nodeId = document.createElement("id");
-//			nodeNumero.appendChild(document.createTextNode(crime.getId()));
-//
-//			crimeElement.appendChild(nodeNumero); 
-//			crimeElement.appendChild(nodeId); 
-//
-//			root.appendChild(crimeElement);
-//			
-//			
-//			
-//			crime.appendChild(localizacao);
-//			
-//			// CRIA NOVO ELEMENTO
-//			Element novoElemento = doc.createElement("novoElemento");
-//			novoElemento.appendChild(doc.createTextNode(data[0])); // ADICIONAR DADOS DO CSV NO XML
-//
-//			// ADICIONA NOVO ELEMENTO COMO FILHO DO ELEMENTO ROOT(PRINCIPAL)		
-//			root.appendChild(novoElemento);
+			Element localizacao = document.createElement("localizacao"); 
+			
+			crime.appendChild(createElement(document, "id", data[0])); 
+			crime.appendChild(createElement(document, "location_description", data[1]));
+			crime.appendChild(createElement(document, "community_area", data[2]));
+			crime.appendChild(createElement(document, "x_coordinate", data[3]));
+			crime.appendChild(createElement(document, "y_coordinate", data[4]));
+			crime.appendChild(createElement(document, "latitude", data[5]));
+			crime.appendChild(createElement(document, "longitude", data[6]));
+			crime.appendChild(createElement(document, "location", data[7]));
+			crime.appendChild(createElement(document, "id_loc", data[8]));
+			
+	
+			crime.appendChild(localizacao);
+
+			raiz.appendChild(crime);
+
+
 		}
 	}
 	private static Element createElement(Document doc, String name, String value) {
