a8d588f302c4e7078e5dcca7dd2a351d48695735
Icety <icetylucas@alu.ufc.br>
2019-09-25 00:08:49 -0300

    lendo csv

--- src/com/br/Aplicacao.java
+++ src/com/br/Aplicacao.java
@@ -1,6 +1,9 @@
 package com.br;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -11,43 +14,59 @@ import com.br.metodos.TransformarFiles;
 
 public class Aplicacao {
 	
-	
-	private static String ARQUIVO ;
-	
-	private static String FILE_CSV = "D:\\Git\\Java-Persistencia\\files\\chicago-crimes.csv";
-	private static String FILE_XML = "D:\\Git\\Java-Persistencia\\files\\chicago-crimes.xml";
-
-	
-	private static LerFiles leitor;
-	private static TransformarFiles transformar;
-	
 	public static void main(String[] args) {
 
+		String FILE_CSV = "/home/lucas/git/persistencia/Java-Persistencia/files/chicago-crimes.csv";
 		
-		leitor = new LerFiles();
-		transformar = new TransformarFiles();
-
-//		entidade = "livro";
-//		raiz_XML = "acervo";
+		BufferedReader conteudoCSV = null;
 		
-		String delimitador = ",";
+		String linha = "";
 		
+		String csvSeparatorCampo = ",";
 		
-		leitor.lerCSV(FILE_CSV, delimitador);
+		try {
+			
+			conteudoCSV = new BufferedReader(new FileReader(FILE_CSV));
+			
+			while ((linha = conteudoCSV.readLine()) != null) {
+				
+				String[] moeda = linha.split(csvSeparatorCampo);
+				
+				System.out.println("[Data = " + moeda[0]
+						+ " , Col = 1 " + moeda[1]
+						+ " , Col = 2 " + moeda[2]
+						+ " , Col = 3 " + moeda[3]
+						+ " , Col = 4 " + moeda[4]
+						+ " , Col = 5 " + moeda[5]
+						+ " , Col = 6 " + moeda[6]
+						+ " , Col = 7 " + moeda[7]
+						+ " , Col = 8 " + moeda[8]
+						+ " , Col = 9 " + moeda[9]
+						+ " , Col = 10 " + moeda[10]
+						+ " , Col = 11 " + moeda[11]
+						+ " , Col = 12 " + moeda[12]
+						+ " , Col = 13 " + moeda[13]
+						+ " , Col = 14 " + moeda[14]
+						+ " , Col = 15 " + moeda[15] + "]");						
+			} 
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("Arquivo não encontrado: \n" + e.getMessage());
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("IndexOutOfBounds: \n" + e.getMessage());
+		} catch (IOException e) {
+			System.out.println("IO Erro	: \n" + e.getMessage());
+		} finally {
+			if (conteudoCSV != null) {
+				try {
+					conteudoCSV.close();
+				} catch (IOException e) {
+					System.out.println("IO Erro: \n" + e.getMessage());
+				}
+			}
+		}
 		
 	}
 	
-	public static void exemplo12LerProperties() throws IOException {
-		Properties prop = new Properties();
-		prop.load(new FileInputStream(ARQUIVO));
-		
-		String aluno = prop.getProperty("aluno");
-		String faculdade = prop.getProperty("faculdade");
-		double salario = Double.parseDouble(prop.getProperty("salario"));
-		
-		
-		System.out.println(aluno);
-		System.out.println(faculdade);
-		System.out.println(salario);
-	}
+	
 }
